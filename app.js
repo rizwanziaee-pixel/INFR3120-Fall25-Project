82a666625aa59d1386f57d606096d3e8daccf6c6
rizwanziaee-pixel <rizwan.ziaee@ontariotechu.net>
2025-11-16 17:47:02 -0500

    6th commit

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,22 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   
   res.status(err.status || 500);
   res.render('error');
 });
-
-// DON'T call app.listen here — bin/www will start the server.
-// app.listen(3000);  <-- remove this
-
 
 const connectDB = require('./db');
 connectDB();
