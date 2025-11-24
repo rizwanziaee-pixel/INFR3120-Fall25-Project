eef000d48a6764f3e0f14a1cb5019f00a0921e05
Yinka-onab <onabanjoiseoluwa@gmail.com>
2025-11-24 18:42:14 -0500

    addel the model user and updated the app.js file to accomodate it

---
+++ README.md
@@ -0,0 +1,59 @@
+# INFR3120-Fall25-Project       https://tasknest-inc7.onrender.com/
+Group 34,  Osman Bilal - 100977834, Rizwan Ziaee - 100972962, Yinka Onabanjo Iseoluwanimi - 100963897
+
+https://github.com/rizwanziaee-pixel/INFR3120-Fall25-Project.git
+
+Name
+Student ID 
+Contributions
+
+
+
+
+Yinka Onabanjo Iseoluwanimi
+
+
+
+
+100963897
+Led front-end development and overall design.
+Managed deployment setup.
+Implemented responsive UI/UX elements
+Organized project assets.
+Created initial CSS styling.
+Built CRUD functionality (Create, Read, Update, Delete).
+Contributed to dashboard/table layout.
+Added responsive design features.
+
+
+
+
+Osman Bilal
+
+
+
+
+100977834
+Assisted with project folder structure and workflow planning.
+Created breakdowns for the video to make planning easier.
+Edited the video to optimize quality.
+Helped Rizwan with debugging.
+Assisted in deploying the project to GitHub.
+
+
+
+
+Rizwan Ziaee
+
+
+
+100972962
+Designed website logo and branding.
+Developed project concept ideas.
+Tested functionality and fixed bugs.
+Built header, footer, and landing page code.
+Assisted with repository setup.
+Made majority of GitHub commits.
+
+
+
---
+++ app.js
@@ -0,0 +1,54 @@
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const connectDB = require('./db');
+connectDB();
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+app.use(
+    session({
+        secret: "tasknestSecret123",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
+    })
+);
+
+
+module.exports = app;
